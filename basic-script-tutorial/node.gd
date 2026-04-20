8f2a5131799277fa4e7374647e93232a75aa22f1
JaeHwan <capitalism0926@gmail.com>
2026-04-21 00:12:16 +0900

    Add files via upload

---
+++ basic-script-tutorial/node.gd
@@ -0,0 +1,15 @@
+extends Node
+
+enum Directions { North, South, East, West}
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var data
+	
+	data = Directions.East
+	
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta) -> void:
+	pass
